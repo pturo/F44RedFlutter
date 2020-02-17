b8e135929248e66e5d719841dc18f2b63ec39bd4
Paweł Turoń <34440020+pturo@users.noreply.github.com>
2020-02-17 18:39:08 +0100

    Update swipe to refresh feature

--- lib/f44red/f44red_main_page.dart
+++ lib/f44red/f44red_main_page.dart
@@ -11,6 +11,10 @@ class F44RedHome extends StatefulWidget {
 }
 
 class F44RedHomeState extends State<F44RedHome> {
+  // Swpipe to refresh feature
+  final GlobalKey<RefreshIndicatorState> refreshIndicatorKey =
+  new GlobalKey<RefreshIndicatorState>();
+
   // Base URL for F44Red page
   final String f44Url = "http://f44red.com/wp-json/wp/v2/";
 
@@ -30,7 +34,7 @@ class F44RedHomeState extends State<F44RedHome> {
     return "Success!";
   }
 
-  Future<Null> _refresh() {
+  Future<Null> refresh() {
     return getPosts().then((post) {
       setState(() => post = posts as String);
     });
@@ -47,86 +51,90 @@ class F44RedHomeState extends State<F44RedHome> {
     return new Scaffold(
       appBar: AppBar(title: Text("F44Red"), backgroundColor: Colors.redAccent),
       drawer: DrawerNav(),
-      body: ListView.builder(
-          itemCount: posts == null ? 0 : posts.length,
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-              onTap: () { Navigator.push(context, new MaterialPageRoute(builder: (context) => new F44RedPost(post: posts[index])));},
-              child: Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Material(
-                  elevation: 14.0,
-                  borderRadius: BorderRadius.circular(10.0),
-                  shadowColor: Theme.of(context).primaryColor.withOpacity(.5),
-                  child: SizedBox(
-                    height: 200.0,
-                    width: MediaQuery.of(context).size.width,
-                    child: Stack(
-                      children: <Widget>[
-                        FadeInImage.assetNetwork(
-                            placeholder: 'images/placeholder.png',
-                            image: posts[index]["featured_media"] == 0
-                                ? 'images/placeholder.png'
-                                : posts[index]["_embedded"]["wp:featuredmedia"][0]
-                            ["source_url"],
-                            width: MediaQuery.of(context).size.width,
-                            fit: BoxFit.cover),
-                        Positioned(
-                          bottom: 0,
-                          child: Container(
-                            decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                    begin: Alignment.bottomCenter,
-                                    end: Alignment.topCenter,
-                                    colors: [
-                                      Colors.black,
-                                      Colors.transparent
-                                    ])),
-                            width: MediaQuery.of(context).size.width,
-                            child: Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 15.0,
-                                  bottom: 5.0,
-                                  left: 5.0,
-                                  right: 5.0),
-                              child: Column(
-                                children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.all(3.0),
-                                    child: Text(
-                                        posts[index]["title"]["rendered"],
-                                        textAlign: TextAlign.left,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 18.0,
-                                          shadows: <Shadow>[
-                                            Shadow(
-                                              offset: Offset(1.0, 1.0),
-                                              blurRadius: 5.0,
-                                              color: Colors.black,
-                                            ),
-                                            Shadow(
-                                              offset: Offset(1.0, 1.0),
-                                              blurRadius: 8.0,
-                                              color: Colors.black,
-                                            ),
-                                          ],
-                                        )
+      body: RefreshIndicator(
+        key: refreshIndicatorKey,
+        onRefresh: refresh,
+        child: ListView.builder(
+            itemCount: posts == null ? 0 : posts.length,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () { Navigator.push(context, new MaterialPageRoute(builder: (context) => new F44RedPost(post: posts[index])));},
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Material(
+                    elevation: 14.0,
+                    borderRadius: BorderRadius.circular(10.0),
+                    shadowColor: Theme.of(context).primaryColor.withOpacity(.5),
+                    child: SizedBox(
+                      height: 200.0,
+                      width: MediaQuery.of(context).size.width,
+                      child: Stack(
+                        children: <Widget>[
+                          FadeInImage.assetNetwork(
+                              placeholder: 'images/placeholder.png',
+                              image: posts[index]["featured_media"] == 0
+                                  ? 'images/placeholder.png'
+                                  : posts[index]["_embedded"]["wp:featuredmedia"][0]
+                              ["source_url"],
+                              width: MediaQuery.of(context).size.width,
+                              fit: BoxFit.cover),
+                          Positioned(
+                            bottom: 0,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                      begin: Alignment.bottomCenter,
+                                      end: Alignment.topCenter,
+                                      colors: [
+                                        Colors.black,
+                                        Colors.transparent
+                                      ])),
+                              width: MediaQuery.of(context).size.width,
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 15.0,
+                                    bottom: 5.0,
+                                    left: 5.0,
+                                    right: 5.0),
+                                child: Column(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.all(3.0),
+                                      child: Text(
+                                          posts[index]["title"]["rendered"],
+                                          textAlign: TextAlign.left,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18.0,
+                                            shadows: <Shadow>[
+                                              Shadow(
+                                                offset: Offset(1.0, 1.0),
+                                                blurRadius: 5.0,
+                                                color: Colors.black,
+                                              ),
+                                              Shadow(
+                                                offset: Offset(1.0, 1.0),
+                                                blurRadius: 8.0,
+                                                color: Colors.black,
+                                              ),
+                                            ],
+                                          )
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            );
-          }),
+              );
+            }),
+      ),
     );
   }
 }
